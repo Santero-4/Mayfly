b4ff58e61bec9f430885cb4eba8a6f53bda158d5
Santero-4 <lmbe05.icloud@gmail.com>
2026-08-22 21:27:12 -0400

    Switched to streamed parallelization

--- mayfly.py
+++ mayfly.py
@@ -4,14 +4,27 @@ import feedparser
 import time
 import requests
 import concurrent.futures as threads
+import calendar
 
+
+def convert_times(time_object, raw):
+    if not time_object:
+        print(f"failed on {raw}")
+        return None
+    else:
+        try:
+            epoch = calendar.timegm(time_object)
+            return time.strftime("%m-%d", time.localtime(epoch))
+        except (OverflowError, OSError):
+            print(f"error on {raw}")
+            return None
 
 def get_today():
     x = time.strftime("%Y-%m-%d", time.localtime(time.time() - 2*(86400)))
     return x
 
 def get_today_no_year():
-    x = time.strftime("%m-%d", time.localtime(time.time() - 2*(86400)))
+    x = time.strftime("%m-%d", time.localtime(time.time() - 0*(86400)))
     return x
 
 def load_urls(filename):
@@ -24,35 +37,42 @@ def load_urls(filename):
 def _fetch(url):
     try:
         response = requests.get(url, headers={"User-Agent": "Mayfly RSS reader Alpha Version (+https://github.com/Santero-4/Mayfly)"}, timeout=10)
-        response.raise_for_status()
-        print(response.status_code)
-        print(response.content[:300])
-        return feedparser.parse(response.content)
+        feed =  feedparser.parse(response.content)
+        return feed
     except requests.exceptions.RequestException as e:
         print(f"{url} threw {e}")
         return None
 
 
-def fetch(urls):
+def fetch(urls, today):
     with threads.ThreadPoolExecutor(max_workers=20) as executor:
-        results = executor.map(_fetch, urls)
-    return [r for  r in results if r is not None]
+        futures = {executor.submit(_fetch, url): url for url in urls}
+        for future in threads.as_completed(futures):
+            feed = future.result()
+            if feed is not None:
+                stream_output(feed, today)
 
-def load_feeds(feed_list, today):
-    """Input: list of URLS as strings
-    Output: list of feedparser feeds that have content from today"""
-    output = []
-    for feed in feed_list:
-        up = getattr(feed, "updated_parsed", None)
-        pub = getattr(feed, "published_parsed", None)
-        print(f"updated: {feed.get("updated")}, published: {feed.get("published")}")
-        print(f"bozo: {feed.bozo}, {feed.get("bozo_exception")}")
-        last_updated = time.strftime("%Y-%m-%d", time.localtime(time.mktime(up))) if up else None
-        last_published = time.strftime("%Y-%m-%d", time.localtime(time.mktime(pub))) if pub else None
-        if last_updated == today or last_published == today:
-            output.append(feed)
-    print(f"Found {len(output)} feeds that updated today...")
-    return output
+
+def stream_output(feed, today):
+    for entry in feed.entries:
+        date = convert_times(entry.get("published_parsed"), entry.get("published"))
+        if date is None:
+            date = convert_times(entry.get("updated_parsed"), entry.get("updated"))
+            if date is None:
+                print("Unprocessable dates:", (entry.get("updated"), entry.get("published")))
+                break
+
+        if date == today:
+            print(f"{entry.get('title')}: ({entry.get('link')}), posted {date}")
+        else:
+            #print(f"{feed.feed.title}, last seen on {date}")
+            break
+    return
+
+
+
+
+
 
 def load_feeds_slowly(urls, today):
     """Input: list of URLS as strings
@@ -145,15 +165,12 @@ def retrive_content(feed_list, today):
 
 def main():
     print("hello world")
-    today = get_today()
-    # urls = load_urls("list.txt")
+    today = get_today_no_year()
+    urls = load_urls("list.txt")
+    #
+    # #fast mode
+    fetch(urls, today)
 
-    #fast mode
-    # feeds = fetch(urls)
-    # filter1 = load_feeds(feeds, today)
-    # content = retrive_content(filter1, get_today_no_year())
-    # for item in content:
-    #     print(item)
 
     #slow mode--debug
     # feeds = load_feeds_slowly(urls, today)
@@ -161,18 +178,26 @@ def main():
     # for item in content:
     #     print(item)
 
-    debug_fast = _fetch("https://daringfireball.net/feeds/main")
-    print(debug_fast.get("updated_parsed"))
-    print(debug_fast.get("published_parsed"))
+    # debug_fast = _fetch("https://foreach.neocities.org/rss.xml")
 
-    debug_slow = feedparser.parse("https://daringfireball.net/feeds/main")
-    print(debug_slow.get("updated_parsed"))
-    print(debug_slow.get("published_parsed"))
+    # print(debug_fast.entries[0].get("updated"))
+    # print(debug_fast.entries[0].get("published"))
+    #
+    # debug_fast = _fetch("https://daringfireball.net/feeds/main")
+    # print(debug_fast.feed.title)
+    # print(debug_fast.entries[0].get("published"))
+    #
+    # fetch(["https://daringfireball.net/feeds/main"], today)
+    #fetch(["https://janlukas.blog/.rss"], today)
+    # debug_slow = feedparser.parse("https://kevquirk.com/feed")
+    # print(debug_slow)
+    # print(debug_slow.entries[0].get("published"))
 
 
 
 if __name__ == "__main__":
     main()
+
 
 
 
