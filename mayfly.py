0dfc94c7c2ceb3cf577f03f7303bc2c9ce4730ee
Santero-4 <lmbe05.icloud@gmail.com>
2026-08-23 11:50:33 -0400

    Main engine complete

--- mayfly.py
+++ mayfly.py
@@ -14,8 +14,6 @@ def date_handler_1(date_string):
     except ValueError:
         return None
 
-
-
 def date_handler_2(date_string):
     try:
         dt = datetime.datetime.strptime(date_string, "%B %d, %Y")
@@ -32,9 +30,6 @@ def date_handler_3(date_string):
     except ValueError:
         return None
 
-
-
-
 def convert_times(time_object, raw):
     if not time_object:
         print(f"failed on {raw}")
@@ -46,10 +41,6 @@ def convert_times(time_object, raw):
         except (OverflowError, OSError):
             print(f"error on {raw}")
             return None
-
-def get_today():
-    x = time.strftime("%Y-%m-%d", time.localtime(time.time() - 2*(86400)))
-    return x
 
 def get_today_no_year():
     x = time.strftime("%m-%d", time.localtime(time.time() - 1*(86400)))
@@ -71,7 +62,6 @@ def _fetch(url):
         print(f"{url} threw {e}")
         return None
 
-
 def fetch(urls, today):
     with threads.ThreadPoolExecutor(max_workers=20) as executor:
         futures = {executor.submit(_fetch, url): url for url in urls}
@@ -79,7 +69,6 @@ def fetch(urls, today):
             feed = future.result()
             if feed is not None:
                 stream_output(feed, today)
-
 
 def stream_output(feed, today):
     for entry in feed.entries:
@@ -97,138 +86,15 @@ def stream_output(feed, today):
             break
     return
 
-
-
-
-
-
-def load_feeds_slowly(urls, today):
-    """Input: list of URLS as strings
-    Output: list of feedparser feeds that have content from today"""
-    output = []
-    for url in urls:
-        feed = feedparser.parse(url)
-        up = getattr(feed, "updated_parsed", None)
-        pub = getattr(feed, "published_parsed", None)
-        last_updated = time.strftime("%Y-%m-%d", time.localtime(time.mktime(up))) if up else None
-        last_published = time.strftime("%Y-%m-%d", time.localtime(time.mktime(pub))) if pub else None
-        if last_updated == today or last_published == today:
-            output.append(feed)
-    print(f"Found {len(output)} feeds that updated today...")
-    return output
-
-
-
-def retrive_content(feed_list, today):
-    """Input: list of feedparser feeds that have content from today
-    Output: list where each item is a tuple (title, full text)"""
-    output = []
-    for feed in feed_list:
-        for entry in feed.entries:
-            up = getattr(entry, "updated_parsed", None)
-            pub = getattr(entry, "published_parsed", None)
-            try:
-                last_updated = time.strftime("%m-%d", time.localtime(time.mktime(up))) if up else None
-            except OverflowError:
-                last_updated = None
-                print(f"date error for {up}")
-            try:
-                published = time.strftime("%m-%d", time.localtime(time.mktime(pub))) if pub else None
-            except OverflowError:
-                published = None
-                print(f"date error for {pub}")
-
-            if (published == today) or (published is None and last_updated == today):
-                if 'title' in entry:
-                    title = entry.title
-                elif 'title' in feed:
-                    title = feed.title
-                else:
-                    title = "Couldn't get a title"
-
-
-                if 'link' in entry:
-                    full_text = entry.link
-                else:
-                    full_text = "We couldn't find anything for this post..."
-                output.append((title, full_text))
-
-            elif (published is None and last_updated is None):
-                if 'title' in entry:
-                    title = entry.title
-                elif 'title' in feed:
-                    title = feed.title
-                else:
-                    title = "Couldn't get a title"
-
-
-                if 'link' in entry:
-                    full_text = entry.link
-                else:
-                    full_text = "We couldn't find anything for this post..."
-                output.append((f"{title}, (undated)", full_text))
-                break
-            else:
-                #uncomment for debug
-                if 'title' in entry:
-                    title = entry.title
-                elif 'title' in feed:
-                    title = feed.title
-                else:
-                    title = "Couldn't get a title"
-
-
-                if 'link' in entry:
-                    full_text = entry.link
-                else:
-                    full_text = "We couldn't find anything for this post..."
-                output.append((f"{title}, Published: {published}", full_text))
-                break
-
-    if len(output) == 0:
-        print("No content found for today")
-        output.append(("test1", "test2"))
-    return output
-
-
 def main():
-    print("hello world")
     feedparser.registerDateHandler(date_handler_1)
     feedparser.registerDateHandler(date_handler_2)
     feedparser.registerDateHandler(date_handler_3)
+
     today = get_today_no_year()
     urls = load_urls("list.txt")
-    #
-    # #fast mode
+
     fetch(urls, today)
-
-
-    #slow mode--debug
-    # feeds = load_feeds_slowly(urls, today)
-    # content = retrive_content(feeds, get_today_no_year())
-    # for item in content:
-    #     print(item)
-
-    # debug_fast = _fetch("https://foreach.neocities.org/rss.xml")
-
-    # print(debug_fast.entries[0].get("updated"))
-    # print(debug_fast.entries[0].get("published"))
-    #
-    # debug_fast = _fetch("https://daringfireball.net/feeds/main")
-    # print(debug_fast.feed.title)
-    # print(debug_fast.entries[0].get("published"))
-    #
-    # fetch(["https://daringfireball.net/feeds/articles"], today)
-    # fetch(["https://daringfireball.net/feeds/json"], today)
-    # debug_slow = feedparser.parse("https://kevquirk.com/feed")
-    # print(debug_slow)
-    # print(debug_slow.entries[0].get("published"))
-
-
 
 if __name__ == "__main__":
     main()
-
-
-
-
